0c510faf9a70a8edfa3dd4d36a98bab7536ed955
matheostronock <matheo.stronock@gmail.com>
2026-03-20 11:54:48 +0100

    main

---
+++ main.py
@@ -0,0 +1,56 @@
+import numpy as np
+import urllib.request
+import urllib.error
+import gzip
+import os
+import ssl
+import time
+import matplotlib.pyplot as plt
+
+def download_mnist(path="./mnist"):
+    pass
+def relu(x):
+    pass
+def relu_backward(gradient, x):
+    pass
+def softmax(x):
+    pass
+def cross_loss(resultatss, y_true):
+    pass
+def train(model, X_train, y_train, X_test, y_test, epochs=5, batch_size=64):
+    pass
+class Conv2D:
+    def __init__(self, n_filters, filter_size, n_channels, lr=0.01):
+        pass
+    def forward(self, entree):
+        pass
+    def backward(self, gradient):
+        pass
+    def _adam_update(self, dW, db):
+        pass
+class MaxPooling2D:
+    def __init__(self, pool_size=2):
+        pass
+
+    def forward(self, entree):
+        pass
+    def backward(self, gradient):
+        pass
+class Dense:
+    def __init__(self, nentree, nsortie, lr=0.01):
+        pass
+    def forward(self, X):
+        pass
+    def backward(self, gradient):
+        pass
+    def _adam_update(self, dW, db):
+        pass
+class CNN:
+    def __init__(self, lr=0.001):
+        pass
+    def forward(self, X):
+        pass
+    def backward(self, gradientscore):
+        pass
+    def predict(self, X):
+        pass
